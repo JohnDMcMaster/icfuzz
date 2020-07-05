e918942367a053ff896bd8b1305c20fcc9347532
John McMaster <johndmcmaster@gmail.com>
2020-07-04 23:40:28 -0700

    patgen: add rand options
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- patgen/pal/pa7140t.py
+++ patgen/pal/pa7140t.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 import struct
+import random
 
 SZ = 0xACB
-f = open('pa7140t_c16_%05u_0x%04X.bin' % (SZ, SZ), 'wb')
+prefix = "pa7140t"
+
+f = open('%s_c16_%05u_0x%04X.bin' % (prefix, SZ, SZ), 'wb')
 
 for i in range(0, SZ, 2):
     f.write(struct.pack('>H', i))
+
+
+buff = bytearray()
+for i in range(0, SZ):
+    buff += struct.pack('>B', random.randint(0, 255))
+firstword = struct.unpack(">I", buff[0:4])[0]
+open('%s_rnd_%04x.bin' % (prefix, firstword), 'wb').write(buff)
 
--- patgen/pal/palce20v8h-15.py
+++ patgen/pal/palce20v8h-15.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 import struct
+import random
 
 SZ = 0x153
-f = open('palce20v8h-15_c16_%05u_0x%04X.bin' % (SZ, SZ), 'wb')
 
+f = open('palce20v8h-15_c16_%05u_0x%04X.bin' % (SZ, SZ), 'wb')
 for i in range(0, SZ, 2):
     f.write(struct.pack('>H', i))
+
+
+buff = bytearray()
+for i in range(0, SZ):
+    buff += struct.pack('>B', random.randint(0, 255))
+open('palce20v8h-15_rnd_%04x.bin' % (struct.unpack(">I", buff[0:4]))[0], 'wb').write(buff)
 
